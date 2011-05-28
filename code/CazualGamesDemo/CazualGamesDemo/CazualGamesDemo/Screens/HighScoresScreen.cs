5e24efaedb4722d85a3e1fdab77d49e56cc26605
fernando.zamora.jimenez@gmail.com <fernando.zamora.jimenez@gmail.com@86484a28-76d8-522f-f7b8-148e2d8275d5>
2011-05-28 20:50:11 +0000

    Updated framework with additional functionality GameScreen now handles the back and exit button presses Added additional screens to demo game

---
+++ code/CazualGamesDemo/CazualGamesDemo/CazualGamesDemo/Screens/HighScoresScreen.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using CazualGames.FrameWork;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CazualGamesDemo.Screens
+{
+    public class HighScoresScreen : GameScreen
+    {
+        private SpriteFont _scoresFont;
+
+        public override void LoadContent(ContentManager content)
+        {
+             _scoresFont = content.Load<SpriteFont>("Scores_font");
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            LoadContent(this.Game.Content);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+
+            DrawHardCodedScores(spriteBatch);
+        }
+
+        private void DrawHardCodedScores(SpriteBatch spriteBatch)
+        {
+            Vector2 fontSize = _scoresFont.MeasureString("W");
+
+            fontSize.Y += 5;
+
+            Func<int,float, float, float> computeHeight =
+                (lineIndex, fontHeight, offset) =>
+                    {
+                        return (lineIndex*fontHeight) + offset;
+                    }; 
+
+            //Hard coded scores
+            spriteBatch.DrawString(_scoresFont, string.Format("{0}{1}", "Joe".PadRight(10), "1024".PadLeft(6)), 
+                                   new Vector2(50, computeHeight(1, fontSize.Y, 100f)), Color.White);
+            spriteBatch.DrawString(_scoresFont, string.Format("{0}{1}", "Jon".PadRight(10), "512".PadLeft(6)),
+                                   new Vector2(50, computeHeight(2, fontSize.Y, 100f)), Color.White);
+            spriteBatch.DrawString(_scoresFont, string.Format("{0}{1}", "Mary".PadRight(10), "256".PadLeft(6)),
+                                   new Vector2(50, computeHeight(3, fontSize.Y, 100f)), Color.White);
+            spriteBatch.DrawString(_scoresFont, string.Format("{0}{1}", "Jay".PadRight(10), "128".PadLeft(6)),
+                                   new Vector2(50, computeHeight(4, fontSize.Y, 100f)), Color.White);
+            spriteBatch.DrawString(_scoresFont, string.Format("{0}{1}", "Travis".PadRight(10), "64".PadLeft(6)),
+                                   new Vector2(50, computeHeight(5, fontSize.Y, 100f)), Color.White);
+            spriteBatch.DrawString(_scoresFont, string.Format("{0}{1}", "Steve".PadRight(10), "32".PadLeft(6)),
+                                   new Vector2(50, computeHeight(6, fontSize.Y, 100f)), Color.White);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            HandleBackToPreviousScreenButton<StartScreen>(false);
+            base.Update(gameTime);
+        }
+    }
+}
---
+++ code/CazualGamesDemo/CazualGamesDemo/CazualGamesDemo/Screens/LevelScreen.cs
@@ -0,0 +1,40 @@
+﻿using CazualGames.FrameWork;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CazualGamesDemo.Screens
+{
+    public class LevelScreen : GameScreen
+    {
+        private Texture2D _carTexture;
+        private int _speed = 5;
+        private int _xLocation = 400;
+
+        public override void Update(GameTime gameTime)
+        {
+            HandleBackToPreviousScreenButton<StartScreen>(true);
+
+            _xLocation -= _speed;
+
+            if (_xLocation + _carTexture.Width < -5)
+                _xLocation = this.Game.GraphicsDevice.Viewport.Width;
+
+
+            base.Update(gameTime);
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+            _carTexture = content.Load<Texture2D>("red_car");
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_carTexture, new Vector2(_xLocation, 300), Color.White);
+        
+            base.Draw(spriteBatch);
+        }
+    }
+}
